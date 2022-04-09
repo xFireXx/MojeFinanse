926861f4b2e5f0cb5d8d4054fcec9ee44e263ca3
Maciej Treder <treder.mc@gmail.com>
2022-04-09 14:06:41 +0200

    add addeventlistener

--- index.js
+++ index.js
@@ -1,5 +1,33 @@
+const name = document.querySelector("#name");
+const category = document.querySelector("#category");
+const date = document.querySelector("#date");
+const price = document.querySelector("#price");
 
-const sqlite3 = require(sqlite3).verbose();
+const btn = document.querySelector("#btn");
+const form = document.querySelector(".inputs");
 
-const db = new sqlite3.Database('./finanse.db', sqlite3.OPEN_READWRITE, (err));
+
+form.addEventListener("submit", e => {
+    const nameValue = name.value;
+    const categoryValue = category.value;
+    const dateValue = date.value;
+    const priceValue = price.value;
+    const tdAdd = document.createElement("tr");
+    tdAdd.innerHTML = `
+                         <td>${nameValue}</td>
+                         <td>${categoryValue}</td>
+                         <td>${priceValue} zł</td>
+                         <td>${dateValue}</td>
+                       `;
+
+    const tableAppendChild = document.querySelector(".tableAppendChild");
+    const tableAppendStatistic = document.querySelector(".tableAppendStatistic");
+
+    tableAppendChild.appendChild(tdAdd);
+
+    e.preventDefault();
+    console.log(name);
+});
+
+
 
